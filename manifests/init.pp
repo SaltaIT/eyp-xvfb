5977a443d376dbbaec5fe33cdfb3ea5b40e40958
Jordi Prats <jprats@systemadmin.es>
2017-01-10 16:38:25 +0100

    listen_tcp default to false

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@ class xvfb(
                             $manage_docker_service = true,
                             $service_ensure        = 'running',
                             $service_enable        = true,
-                            $listen_tcp            = true,
+                            $listen_tcp            = false,
                           ) inherits xvfb::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
